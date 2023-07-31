6725cf428e2d1588f0695523878a7c9da473c2e8
eenblam <eenblam@riseup.net>
2023-07-31 11:20:38 -0700

    Tidy up templating

--- views.go
+++ views.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -15,6 +17,11 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Poser</h1><form action=\"/\" method=\"POST\"><input type=\"submit\" value=\"New Room\"></form>"))
 }
 
+// Could make this t := template.Must(template.ParseGlob("./templates/*.html"))
+// then do t.ExecuteTemplate(w, "room.html", <data>)
+// if more templates get added
+var roomHTMLTemplate = template.Must(template.ParseFiles("./templates/room.html"))
+
 func NewRoomHandler(w http.ResponseWriter, r *http.Request) {
 	// Homepage requested a new room
 	//TODO could instead do some sort of TinyURL style Base58(SHA256(url, username))
@@ -26,20 +33,15 @@ func NewRoomHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, roomPath, http.StatusFound)
 }
 
-var roomHTMLTemplate = `<title>Room: %v</title>
-<body>
-<p>Room ID: %v</p>
-<script src="/static/websocket.js"></script>
-</body> `
-
-//<script type="text/javascript" src="/static/websocket.js"></script>
-//<script type="module" src="https://unpkg.com/simple-peer@9.11.1/simplepeer.min.js"></script>
-
 // RoomHandler serves the room assets
 func RoomHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, roomHTMLTemplate, vars["id"], vars["id"])
+	data := struct{ Room string }{Room: vars["id"]}
+	err := roomHTMLTemplate.Execute(w, data)
+	if err != nil {
+		log.Printf("failed to execute template: %s", err)
+	}
 }
 
 // NameHandler allows a user to set their name via the API
