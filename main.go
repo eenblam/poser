bf1bb8b64e015e27a8481f709c51ea81e301c915
eenblam <eenblam@riseup.net>
2023-08-02 11:16:53 -0700

    Drop old /static/ dir

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 )
 
 func main() {
-	// Serve /static/ from ./static/
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("frontend/dist/assets/"))))
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler).Methods("GET")
 	r.HandleFunc("/", NewRoomHandler).Methods("POST")
-	//r.HandleFunc("/room/{id:[0-9]+}", RoomHandler)
 	r.HandleFunc("/room/{id:.*}", RoomHandler)
 	//r.HandleFunc("/gallery/{id:[0-9]+}", GalleryHandler)
 
