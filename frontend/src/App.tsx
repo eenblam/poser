4b98a66cc4d368bf992a4117c7218b4b180e2b66
eenblam <eenblam@riseup.net>
2023-08-03 14:41:03 -0700

    Refactor App to a functional component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,43 @@
-import { Component } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import UserList from './components/UserList'
 import { Chat, Message } from './components/Chat'
 import WebSocketContext from './WebSocketContext'
 import './App.css'
 
-interface AppState {
-  userId: string;
-  userList: string[];
-  messages: Message[];
-}
+function App() {
+  let [_, setUserId] = useState<string>('...loading...');
+  let [userList, setUserList] = useState<string[]>([]); // how to set type string[]?
+  let [messages, setMessages] = useState<Message[]>([]); // how to set type Message[]?
+  let connRef = useRef<WebSocket | null>(null);
 
-class App extends Component<{}, AppState> {
-  conn: WebSocket;
-
-  constructor(props: AppState) {
-    super(props);
-
-    this.state = {
-      userId: '...loading...',
-      userList: [],
-      messages: [],
-    }
-
-    // Translate http(s)://HOST/room/ID/ to ws(s)://HOST/ws/ID/
+  useEffect(() => {
+        // Translate http(s)://HOST/room/ID/ to ws(s)://HOST/ws/ID/
     let wsProtocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
     let wsUrl = `${wsProtocol}//${location.host}${location.pathname.replace('/room/', '/ws/')}`;
 
-    this.conn = new WebSocket(wsUrl, 'json');
-    this.conn.onopen = (e) => {
+    // let conn = new WebSocket(wsUrl, 'json');
+    let conn = new WebSocket(wsUrl, 'json');
+    conn.onopen = (e) => {
       console.log(`wsConnection open to 127.0.0.1:8080`, e);
     };
-    this.conn.onerror = (e) => {
+    conn.onerror = (e) => {
       console.error(`wsConnection error `, e);
     };
-    this.conn.onmessage = (e) => {
+    conn.onmessage = (e) => {
       let data = JSON.parse(e.data);
       switch (data.type) {
         case 'connection':
-          this.setState({...this.state, userId: data.id});
+          setUserId(data.id);
           break;
         case 'ids':
           console.log(`Ids: ${data.ids}`);
-          this.setState({...this.state, userList: data.ids});
+          setUserList(data.ids);
           break;
         case 'chat':
           let d = data.data
           let m = new Message(d.id, d.user, d.timestamp, d.text);
           console.log(`Chat: ${m.toWSMessage()}`);
-          this.setState({...this.state, messages: [...this.state.messages, m]});
+          setMessages((messages) => [...messages, m]);
           break;
         case undefined:
           console.log("Undefined message type");
@@ -58,20 +48,24 @@ class App extends Component<{}, AppState> {
           console.log(e);
       }
     };
-  }
+    connRef.current = conn;
+    // Return a cleanup function to cleanly close the connection
+    return () => {
+      conn.close();
+      connRef.current = null;
+    };
+  }, []);
 
-
-  render() {
   return (
     <>
       <h1>Poser</h1>
-      <WebSocketContext.Provider value={this.conn}>
-        <UserList users={this.state.userList} />
-        <Chat messages={this.state.messages} />
+      <WebSocketContext.Provider value={connRef.current}>
+        <UserList users={userList} />
+        <Chat messages={messages} />
+        <Canvas/>
       </WebSocketContext.Provider>
     </>
   )
-  }
 }
 
 export default App
