31463f54a71248efd9fdd8a75723f2bb910c464a
eenblam <eenblam@riseup.net>
2023-08-15 11:29:20 -1000

    Move app into a sub-module

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './common.css'
+import '../common.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
